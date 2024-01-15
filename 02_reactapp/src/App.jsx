a0b2749fc238fc1386ada2a0d11852d3fa9b4bd1
IDrumahdev <ibnudirsan@gmail.com>
2024-01-15 14:53:52 +0700

    setup react and taildwincss via Vite

---
+++ 02_reactapp/src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className='flex justify-center bg-blue-600 min-h-screen items-center'>
+      <div>
+          <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
+            Buy now
+          </button>
+
+          <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
+            Buy now
+          </button>
+
+          <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
+            Buy now
+          </button>
+      </div>
+    </div>
+  )
+}
+
+export default App

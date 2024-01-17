202e47c5ff6eac4e800903c1053a0e3ef08bb274
IDrumahdev <ibnudirsan@gmail.com>
2024-01-17 10:45:08 +0700

    Rendering Lists

--- 03_reactapp_atomic_design/src/Pages/products.jsx
+++ 03_reactapp_atomic_design/src/Pages/products.jsx
@@ -1,24 +1,42 @@
 import CardProduct from '../components/Fragments/CardProduct';
+
+const products = [
+    {
+        id: 1,
+        title: "Sepatu Baru",
+        price: 110000,
+        image: "/images/product.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    },
+    {
+        id: 2,
+        title: "Sepatu Bekas",
+        price: 120000,
+        image: "/images/product.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit consequatur repudiandae beatae architecto."
+    },
+    {
+        id: 3,
+        title: "Sepatu KW",
+        price: 130000,
+        image: "/images/product.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam impedit fugit amet quasi placeat consequatur repudiandae beatae architecto."
+    }
+]
+
 const Products = () => {
 
     return (
         <div className="flex justify-center py-5">
-            <CardProduct>
-                <CardProduct.Header Image="/images/product.jpg"></CardProduct.Header>
-                <CardProduct.Body title ="Lorem ipsum">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Aliquam impedit fugit amet quasi placeat consequatur repudiandae beatae architecto.
-                </CardProduct.Body>
-                <CardProduct.Footer price="Rp. 100.000" />
-            </CardProduct>
-            <CardProduct>
-                <CardProduct.Header Image="/images/product.jpg"></CardProduct.Header>
-                <CardProduct.Body title ="Lorem ipsum">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Aliquam impedit fugit amet quasi placeat consequatur repudiandae beatae architecto.
-                </CardProduct.Body>
-                <CardProduct.Footer price="Rp. 100.000" />
-            </CardProduct>
+            {products.map((product) => (
+                <CardProduct key={product.id}>
+                    <CardProduct.Header Image={product.image}></CardProduct.Header>
+                    <CardProduct.Body title={product.title}>
+                        {product.description}
+                    </CardProduct.Body>
+                    <CardProduct.Footer price={`Rp. ${product.price}`} />
+                </CardProduct>
+            ))}
         </div>
     )
 }

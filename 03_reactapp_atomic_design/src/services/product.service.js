a6aac1e98c27c93cce0023247ca323dc45207164
IDrumahdev <ibnudirsan@gmail.com>
2024-01-18 17:36:33 +0700

    16_Get_Data_API

--- 03_reactapp_atomic_design/src/services/product.service.js
+++ 03_reactapp_atomic_design/src/services/product.service.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 export const getProduct = (callback) => {
     axios.get(`https://fakestoreapi.com/products`).then((res) => {
-        console.log(res);
         callback(res.data);
     }).catch((err) => {
         console.log(err);
